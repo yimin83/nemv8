bc36d34eed1c64eb4fe2c3143ccf9d212112a33d
yimin83 <yimin83@gmail.com>
2019-09-23 00:49:39 +0800

    20190923 #1 집에서 그래프

--- fe/src/main.js
+++ fe/src/main.js
@@ -5,6 +5,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import 'chart.js'
+import 'hchs-vue-charts'
+import Chartkick from 'chartkick'
+import VueChartkick from 'vue-chartkick' 
+
+Vue.use(window.VueCharts)
+ Vue.use(VueChartkick, { Chartkick })
 
 Vue.config.productionTip = false
 
